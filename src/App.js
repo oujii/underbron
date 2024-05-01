e223048a8fdcaa89355f2a061fd063b36eab912f
oujii <carl.waerner@hyperisland.se>
2024-05-01 20:36:10 +0200

    variables etc1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, useDeferredValue } from 'react';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://script.google.com/macros/s/AKfycbyPlstT4U_fwZwY1AT5seSq6OI3-NpmebHFm6x8bFcW20URDWQtaE23Sqr4PJzgo3TisA/exec");
+        const jsonData = await res.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {data ? (
+          <div>
+            <h1>Data Loaded</h1>
+            <div className="producers">
+              {data.data.map((item, index) => 
+                item.producer ? <div key={index} className="producer">{item.producer}</div> : null
+              )}
+            </div>
+          </div>
+        ) : (
+          <p>Loading data...</p>
+        )}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Re
         </a>
       </header>
     </div>
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRX4ncufSVi8IhJgFPMk2QocDhN9O_CM8",
+  authDomain: "underbron-7be16.firebaseapp.com",
+  databaseURL: "https://underbron-7be16-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "underbron-7be16",
+  storageBucket: "underbron-7be16.appspot.com",
+  messagingSenderId: "928143923917",
+  appId: "1:928143923917:web:13cc4177ddafc3a0a4a784"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
