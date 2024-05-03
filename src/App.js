b2dcf9179f20063f1c6eb93eca51a29ecf06b585
oujii <carl.waerner@hyperisland.se>
2024-05-03 22:15:10 +0200

    flaskmeny

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import React, { useState, useEffect } from 'react';
 function App() {
 
   const [data, setData] = useState(null);
+  const [data2, setData2] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,6 +14,13 @@ function App() {
         const res = await fetch("https://script.google.com/macros/s/AKfycbzZVxfujSYR7sF0Ord3QRyhpZ4tvs7fcTVJoyWkLzUkL1SJe51vY1__1SakB50bTCGyyA/exec");
         const jsonData = await res.json();
         setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+      try {
+        const res2 = await fetch("https://script.google.com/macros/s/AKfycbyWP2z0DNE0-rB-quNqIZAIP7hA6KhxzDiqqa6w9GXsok6Rx5YEMnI2VEOqpk1wo1BVsA/exec");
+        const jsonData2 = await res2.json();
+        setData2(jsonData2);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -34,6 +42,8 @@ function App() {
   let fvittRendered = false;
   let frottRendered = false;
   let forangeRendered = false;
+  let fmagnumRendered = false;
+  
 
 
   return (
@@ -109,10 +119,10 @@ function App() {
                 </div>
               ))}
 
+
+              
+
               <video src={logo} className="App-logo" alt="logo" autoPlay loop /> 
-
-
-
                 
 
 
@@ -121,6 +131,82 @@ function App() {
         ) : (
           <p>Laddar meny...</p>
         )}
+
+
+{data2 ? (
+          <div>
+            <div className="wrapper">
+              {data2.data.map((item, index) => (
+                <div key={index}>
+                 
+                  {/* Render category title if not rendered yet */}
+          {!highlightRendered && item.category === 'Highlight' && (
+            <>
+              <div className="category_title"></div>
+              {highlightRendered = true} {/* Update local variable */}
+            </>
+          )}
+
+          {!fpetnatRendered && item.category === 'Pét Nat' && (
+            <>
+              <div className="category_title">Pét Nat</div>
+              {fpetnatRendered = true} {/* Update local variable */}
+            </>
+          )}
+
+          {!fvittRendered && item.category === 'Vitt' && (
+            <>
+              <div className="category_title">Vitt</div>
+              {fvittRendered = true} {/* Update local variable */}
+            </>
+          )}
+
+           {!frottRendered && item.category === 'Rött' && (
+            <>
+              <div className="category_title">Rött</div>
+              {frottRendered = true} {/* Update local variable */}
+            </>
+          )}
+           {!forangeRendered && item.category === 'Orange' && (
+            <>
+              <div className="category_title">Orange</div>
+              {forangeRendered = true} {/* Update local variable */}
+            </>
+          )}
+             {!fmagnumRendered && item.category === 'Magnum' && (
+            <>
+              <div className="category_title">Magnum</div>
+              {fmagnumRendered = true} {/* Update local variable */}
+            </>
+          )}
+
+                  {/* Render item */}
+                  {(item.category === 'Magnum' || item.category === 'Pét Nat' || item.category === 'Vitt' || item.category === 'Rött'  || item.category === 'Orange' || item.category === 'Geist of the Night'  || item.category === 'Övrigt' || item.category === 'Snacks') && (
+                    <div className="menykategori">
+                      <div className='subcategory'>{item.subcategory} </div>
+                      <div className='subcategory_price'>{item.subcategory_price}</div>
+                      <div className='info'>{item.year} {item.name} <br/><span className='producer'>{item.producer} ({item.region})</span></div>
+                      <div className='price'>{item.price}</div>
+                    </div>
+                  )}
+                </div>
+              ))}
+
+
+              
+
+              <video src={logo} className="App-logo" alt="logo" autoPlay loop /> 
+                
+
+
+            </div>
+          </div>
+        ) : (
+          <p>Laddar meny...</p>
+        )}
+
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
